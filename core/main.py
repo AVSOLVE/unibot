631a81cf3e260fd0bcb4cca5a5438c0c462efa1a
Alexandre Lima <alexandrelima.mail@gmail.com>
2025-01-03 14:23:46 -0300

    sdfsdf

--- core/main.py
+++ core/main.py
@@ -127,9 +127,9 @@ def match_code(code):
 async def login_auth(credentials, page):
     await page.goto(urls["loginPage"], wait_until="domcontentloaded")
     frame = page.frame_locator("iframe >> nth=0").frame_locator("#principal")
-    await frame.locator("#tipoUsuario").select_option("P")
-    await frame.locator("#nmUsuario").fill(credentials["login"])
-    await frame.locator("#dsSenha").fill(credentials["password"])
+    frame.locator("#tipoUsuario").select_option("P")
+    frame.locator("#nmUsuario").fill(credentials["login"])
+    frame.locator("#dsSenha").fill(credentials["password"])
     await frame.get_by_role("button", name="Entrar").click()
     print("Login successful!")
     return await navigate_executar_guia(page)
